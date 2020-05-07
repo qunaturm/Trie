4868bf4a6d48b3b1da3b8189f91b71464b500388
Хисматулина Карина <hkarinaf@gmail.com>
2020-05-07 20:53:56 +0300

    1v

---
+++ Trie/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trie
+{
+    class Node<T>
+    {
+        public string Key { get; private set; }
+        public T Data { get; private set; }
+        public bool Flag { get; set; }
+        public Dictionary<string, Node<T>> SubNodes { get; set; }
+        
+    }
+}
