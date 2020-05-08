874f10825e73c889f68a6bfc32cdc2cc30e2a1ac
Хисматулина Карина <hkarinaf@gmail.com>
2020-05-08 13:40:18 +0300

    v2

--- Trie/Node.cs
+++ Trie/Node.cs
@@ -6,10 +6,42 @@ namespace Trie
 {
     class Node<T>
     {
-        public string Key { get; private set; }
+        public char Symbol { get; private set; }
         public T Data { get; private set; }
         public bool Flag { get; set; }
+        public string Prefix { get; set; } 
         public Dictionary<string, Node<T>> SubNodes { get; set; }
-        
+
+        public Node(T data) //ctor
+        {
+            if (data != null)
+            {
+                Data = data;
+                //Flag = false;
+                
+            }
+        }
+
+        public Node(string key, T data)
+        {
+            Data = data;
+            //Flag = false;
+        }
+
+        public override string ToString() //?????????
+        {
+            return Data.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Node<T> item)
+            {
+                return Data.Equals(item);
+            }
+
+            else return false;
+        }
+
     }
 }
---
+++ Trie/Trie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Trie
+{
+    class Trie<T>
+    {
+        private Node<T> Root { get; set; }
+        public int Count { get; set; }
+
+        public Trie(T data)
+        {
+            if (data != null)
+            {
+                Root = new Node<T>(" ", data);
+                Count = 1;
+            } 
+        }
+        public void Add(string key, T data)
+        {
+            var current = Root;
+            foreach(var sym in key)
+            {
+                if(current.Data != null)
+                {
+                    Add(key, current.Data);
+                }
+            }
+        }
+
+        public void Remove(T data)
+        {
+
+        }
+
+        public T Search(string key)
+        {
+
+        }
+
+    }
+}
